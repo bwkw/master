deb83f2bbac885231900a933d6c07e2440591f4b
S.Naito <na.shota.1139@gmail.com>
2021-08-21 11:53:09 +0900

    単原子系一時終了

--- one_component_system/task/dump.sh
+++ one_component_system/task/dump.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
 cd $PBS_O_WORKDIR
-mpirun -np 8 /home/apps/lammps/lmp_mpi < in.melt/l0.8-r0.0-T0.9.in 
+mpirun -np 8 /home/apps/lammps/lmp_mpi < in.melt/ln108000-rn0-ld0.8-rd0.0-T0.95.in 
