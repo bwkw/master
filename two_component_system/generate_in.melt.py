242ae4abf56f91887beeec8107e8714722ae2fd6
S.Naito <na.shota.1139@gmail.com>
2021-08-22 19:15:43 +0900

    温度変化

--- two_component_system/generate_in.melt.py
+++ two_component_system/generate_in.melt.py
@@ -23,6 +23,6 @@ left_num = 10*10*10*4
 right_num = 10*10*10*4
 left_density = left_num/half_volume
 right_density = right_num/half_volume
-temperature = 0.95
+temperature = 1.15
 volume = (left_num/left_density)*2
 make_file("in.melt/ln{}-rn{}-ld{}-rd{}-T{}.in".format(left_num, right_num, left_density, right_density, temperature), left_num, right_num, left_density, right_density, temperature)
--- two_component_system/task/dump.sh
+++ two_component_system/task/dump.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
 cd $PBS_O_WORKDIR
-mpirun -np 8 /home/apps/lammps/lmp_mpi < in.melt/ln4000-rn4000-ld0.5-rd0.5-T0.95.in 
+mpirun -np 8 /home/apps/lammps/lmp_mpi < in.melt/ln4000-rn4000-ld0.5-rd0.5-T1.15.in 
