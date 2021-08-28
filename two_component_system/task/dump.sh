0479ae80623831c771047ffa48197b2990c77b4f
S.Naito <na.shota.1139@gmail.com>
2021-08-28 21:36:26 +0900

    条件変更

--- two_component_system/task/dump.sh
+++ two_component_system/task/dump.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
 cd $PBS_O_WORKDIR
-mpirun -np 8 /home/apps/lammps/lmp_mpi < in.melt/ln5324-rn5324-ld0.6655-rd0.6655-T1.1.in 
+mpirun -np 8 /home/apps/lammps/lmp_mpi < in.melt/ln5324-rn5324-ld0.6655-rd0.6655-T0.95.in 
