f2e6c17877deecc6b0fccb20ce2f59617409eef4
S.Naito <na.shota.1139@gmail.com>
2021-09-20 09:49:01 +0900

    条件変更

--- two_component_system-gas_liquid/symmetric/generate_in.melt.py
+++ two_component_system-gas_liquid/symmetric/generate_in.melt.py
@@ -16,12 +16,12 @@ def make_file(filename, left_num_a, left_num_b, left_density_a, left_density_b, 
         f.write("pair_coeff 2 2 1.0 1.0 3.0\n\n")
         f.write("fix 1 all nvt temp {} {} 0.01\n\n".format(temperature, temperature))
         f.write("dump id all atom 1000 dump.melt/lna{}-lnb{}-lda{}-ldb{}-T{}.dump\n\n".format(left_num_a, left_num_b, left_density_a, left_density_b, temperature))
-        f.write("run 1000000")
+        f.write("run 100000")
 
 half_volume = 40*40*40
 left_num_a = 22*22*22*4
 left_num_b = 22*22*22*4
 left_density_a = left_num_a/half_volume
 left_density_b = left_num_b/half_volume
-temperature = 1.3
+temperature = 0.9
 make_file("in.melt/lna{}-lnb{}-lda{}-ldb{}-T{}.in".format(left_num_a, left_num_b, left_density_a, left_density_b, temperature), left_num_a, left_num_b, left_density_a, left_density_b, temperature)
