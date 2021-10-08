167cc9ac2ec5c8a09cf3b4c3c62b4aba6f2f9c03
S.Naito <na.shota.1139@gmail.com>
2021-10-09 00:26:16 +0900

    [update]粒子の個数変更

--- two_component_system-gas_liquid/symmetric/generate_in.melt.py
+++ two_component_system-gas_liquid/symmetric/generate_in.melt.py
@@ -15,13 +15,17 @@ def make_file(filename, left_num_a, left_num_b, left_density_a, left_density_b, 
         f.write("pair_coeff 1 2 1.05 0.9 3.0\n")
         f.write("pair_coeff 2 2 1.0 1.0 3.0\n\n")
         f.write("fix 1 all nvt temp {} {} 0.01\n\n".format(temperature, temperature))
+        f.write("run 200000\n\n")
         f.write("dump id all atom 1000 dump.melt/lna{}-lnb{}-lda{}-ldb{}-T{}.dump\n\n".format(left_num_a, left_num_b, left_density_a, left_density_b, temperature))
-        f.write("minimize 0.0 1.0e-16 1000 100000")
+        f.write("run 5000")
 
 half_volume = 40*40*40
-left_num_a = 23*23*23*4
-left_num_b = 21*21*21*4
+left_num = 28*28*28*4
+left_num_a_ratio = 0.5
+left_num_a = round(left_num * left_num_a_ratio)
+left_num_b = left_num - left_num_a
+right_num = 0
 left_density_a = left_num_a/half_volume
 left_density_b = left_num_b/half_volume
-temperature = 0.99
+temperature = 0.95
 make_file("in.melt/lna{}-lnb{}-lda{}-ldb{}-T{}.in".format(left_num_a, left_num_b, left_density_a, left_density_b, temperature), left_num_a, left_num_b, left_density_a, left_density_b, temperature)
