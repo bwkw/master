95e9acced9e8ad24ad1c8dfc35a9c55181006bfa
S.Naito <na.shota.1139@gmail.com>
2021-11-01 10:01:59 +0900

    initial commit

---
+++ two_component_system-gas_liquid/vapor_pressure-temperature_dependence/generate_temperature_sh.py
@@ -0,0 +1,19 @@
+# コマンドライン引数一つ目にtemperature
+
+import sys
+
+def make_file(filename, left_num_a, left_num_b, left_density_a, left_density_b, temperature):
+    with open(filename, "a") as f:      
+        f.write("/home/apps/lammps/lmp_serial < in.melt/T{}/lna{}-lnb{}-lda{}-ldb{}".format(temperature, left_num_a, left_num_b, left_density_a, left_density_b))
+
+temperature = float(sys.argv[1])
+for i in range(1,20):
+    half_volume = 40*40*40
+    left_num = 22*22*22*4
+    left_num_a_ratio = float(i) * 0.05
+    left_num_a = round(left_num * left_num_a_ratio)
+    left_num_b = left_num - left_num_a
+    right_num = 0
+    left_density_a = left_num_a/half_volume
+    left_density_b = left_num_b/half_volume
+    make_file("temperature{}.sh".format(temperature), left_num_a, left_num_b, left_density_a, left_density_b, temperature)
