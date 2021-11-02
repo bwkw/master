686e38929804abb9c9fc665c1ece12d0bb2e06d6
S.Naito <na.shota.1139@gmail.com>
2021-11-02 10:17:07 +0900

    [change]組成割合をコマンドライン引数で指定可能に

--- two_component_system-gas_liquid/vapor_pressure-temperature_dependence/generate_atoms.py
+++ two_component_system-gas_liquid/vapor_pressure-temperature_dependence/generate_atoms.py
@@ -1,4 +1,5 @@
-# atomsファイルを作成
+# コマンドライン引数一つ目で組成割合を何分割するかを指定
+# 例）20と入力すると、0.05,0.1,....0.95
 
 import math
 import random
@@ -78,15 +79,16 @@ def make_file(filename, atoms, length):
 atoms = []
 half_volume = 40*40*40
 left_num = 22*22*22*4
-left_num_a_ratio = float(sys.argv[1])
-left_num_a = round(left_num * left_num_a_ratio)
-left_num_b = left_num - left_num_a
-right_num = 0
-left_density_a = left_num_a/half_volume
-left_density_b = left_num_b/half_volume
-length = round(math.pow(half_volume, 1/3))
-add_ball(atoms, left_num, left_num_a, length)
-make_file("atoms/lna{}-lnb{}-lda{}-ldb{}.atoms".format(left_num_a, left_num_b, left_density_a, left_density_b), atoms, length)
+for i in range(1, int(sys.argv[1])):
+    left_num_a_ratio = (1/int(sys.argv[1])) * int(i)
+    left_num_a = round(left_num * left_num_a_ratio)
+    left_num_b = left_num - left_num_a
+    right_num = 0
+    left_density_a = left_num_a/half_volume
+    left_density_b = left_num_b/half_volume
+    length = round(math.pow(half_volume, 1/3))
+    add_ball(atoms, left_num, left_num_a, length)
+    make_file("atoms/lna{}-lnb{}-lda{}-ldb{}.atoms".format(left_num_a, left_num_b, left_density_a, left_density_b), atoms, length)
 
 # 分子の速度平均が≒0になっていることの確認
 sum_a_vx = 0
--- two_component_system-gas_liquid/vapor_pressure-temperature_dependence/generate_in.melt.py
+++ two_component_system-gas_liquid/vapor_pressure-temperature_dependence/generate_in.melt.py
@@ -1,4 +1,6 @@
-# コマンドライン引数1つ目に組成割合、2つ目に温度
+# コマンドライン引数一つ目で組成割合を何分割するかを指定
+# 例）20と入力すると、0.05,0.1,....0.95
+# コマンドライン引数二つ目で温度を指定
 
 import math
 import sys
@@ -24,11 +26,12 @@ def make_file(filename, left_num_a, left_num_b, left_density_a, left_density_b, 
 
 half_volume = 40*40*40
 left_num = 22*22*22*4
-left_num_a_ratio = float(sys.argv[1])
-left_num_a = round(left_num * left_num_a_ratio)
-left_num_b = left_num - left_num_a
-right_num = 0
-left_density_a = left_num_a/half_volume
-left_density_b = left_num_b/half_volume
-temperature = float(sys.argv[2])
-make_file("in.melt/T{}/lna{}-lnb{}-lda{}-ldb{}.in".format(temperature, left_num_a, left_num_b, left_density_a, left_density_b), left_num_a, left_num_b, left_density_a, left_density_b, temperature)
+for i in range(1, int(sys.argv[1])):
+    left_num_a_ratio = (1/int(sys.argv[1])) * int(i)
+    left_num_a = round(left_num * left_num_a_ratio)
+    left_num_b = left_num - left_num_a
+    right_num = 0
+    left_density_a = left_num_a/half_volume
+    left_density_b = left_num_b/half_volume
+    temperature = float(sys.argv[2])
+    make_file("in.melt/T{}/lna{}-lnb{}-lda{}-ldb{}.in".format(temperature, left_num_a, left_num_b, left_density_a, left_density_b), left_num_a, left_num_b, left_density_a, left_density_b, temperature)
