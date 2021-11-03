2388834bf09d7ed5fdec6b770815138528f4ab4a
S.Naito <na.shota.1139@gmail.com>
2021-11-03 12:07:10 +0900

    [change]ファイル名変更に伴う実装方法の変更

--- two_component_system-gas_liquid/vapor_pressure-temperature_dependence/generate_density_plt.py
+++ two_component_system-gas_liquid/vapor_pressure-temperature_dependence/generate_density_plt.py
@@ -16,7 +16,7 @@ def make_file(filename, left_num_a, left_num_b, left_density_a, left_density_b, 
         f.write("set ylabel font 'Arial,15'\n")
         f.write("set tics font 'Arial,10'\n")
         f.write("set key font 'Arial,16'\n")
-        f.write("plot 'density/T{4}/lna{0}-lnb{1}-lda{2}-ldb{3}.density' u 1:2 title '粒子A', 'density/T{4}/lna{0}-lnb{1}-lda{2}-ldb{3}.density' u 1:3 title '粒子B',\n".format(left_num_a, left_num_b, left_density_a, left_density_b, temperature))
+        f.write("plot 'density_dat/T{4}/lna{0}-lnb{1}-lda{2}-ldb{3}.dat' u 1:2 title '粒子A', 'density_dat/T{4}/lna{0}-lnb{1}-lda{2}-ldb{3}.dat' u 1:3 title '粒子B',\n".format(left_num_a, left_num_b, left_density_a, left_density_b, temperature))
         
 half_volume = 40*40*40
 left_num = 22*22*22*4
@@ -29,9 +29,9 @@ for i in range(1, int(sys.argv[1])):
     left_density_b = left_num_b/half_volume
     temperature = float(sys.argv[2])
 
-    if not os.path.exists('make_plt/T{}'.format(temperature)):
-        os.mkdir(('make_plt/T{}'.format(temperature)))
+    if not os.path.exists('density_make_plt/T{}'.format(temperature)):
+        os.mkdir(('density_make_plt/T{}'.format(temperature)))
     if not os.path.exists('density_img/T{}'.format(temperature)):
         os.mkdir(('density_img/T{}'.format(temperature)))
 
-    make_file("make_plt/T{}/lna{}-lnb{}-lda{}-ldb{}.plt".format(temperature, left_num_a, left_num_b, left_density_a, left_density_b), left_num_a, left_num_b, left_density_a, left_density_b, temperature)
+    make_file("density_make_plt/T{}/lna{}-lnb{}-lda{}-ldb{}.plt".format(temperature, left_num_a, left_num_b, left_density_a, left_density_b), left_num_a, left_num_b, left_density_a, left_density_b, temperature)
