5976f9448a6478093472493886077c5b4c986767
S.Naito <na.shota.1139@gmail.com>
2021-11-04 19:54:31 +0900

    [change]実行ファイル変更

--- one_component_system/task/dump.sh
+++ one_component_system/task/dump.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
 cd $PBS_O_WORKDIR
-mpirun -np 8 /home/apps/lammps/lmp_mpi < in.melt/ln42592-rn0-ld0.6655-rd0-T1.0.in 
+mpirun -np 8 /home/apps/lammps/lmp_mpi < input/ln42592-rn0-ld0.6655-rd0-T1.0.input > output/ln42592-rn0-ld0.6655-rd0-T1.0.output 
