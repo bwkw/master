643083cc0712ae6b6fd2d75e15f7c29209105c94
S.Naito <na.shota.1139@gmail.com>
2021-11-04 18:47:14 +0900

    [update]粒子数増加

--- one_component_system/generate_input.py
+++ one_component_system/generate_input.py
@@ -1,26 +1,29 @@
 import math
 
-def make_file(filename, left_density, right_density, temperature):
+def make_file(filename, left_num, right_num, left_density, right_density, temperature):
     with open(filename, "w") as f:
         f.write("units lj\n")
         f.write("atom_style atomic\n")
         f.write("boundary p p p\n\n")
-        f.write("read_data atoms/l{}-r{}.atoms\n\n".format(left_density, right_density))
+        f.write("read_data atoms/ln{}-rn{}-ld{}-rd{}.atoms\n\n".format(left_num, right_num, left_density, right_density))
         f.write("mass 1 1.0\n\n")
         f.write("reset_timestep  0\n")
         f.write("timestep        0.001\n\n")
         f.write("pair_style lj/cut 3.0\n")
         f.write("pair_coeff 1 1 1.0 1.0 3.0\n\n")
         f.write("fix 1 all nvt temp {} {} 0.01\n\n".format(temperature,temperature))
-        f.write("thermo 10\n")
+        f.write("fix 1 all nvt temp {0} {0} 0.01\n\n".format(temperature))
+        f.write("run 200000\n\n")
+        f.write("thermo 1000\n")
         f.write("thermo_style custom time temp press\n\n")
-        f.write("run 20000")
+        f.write("run 5000")
 
 
-left_num = 6*6*6*4
+half_volume = 40*40*40
+left_num = 22*22*22*4
 right_num = 0
-left_density = 0.8
-right_density = 0.0
-temperature = 0.9
-volume = (left_num/left_density)*2
-make_file("input/l{}-r{}-T{}.input".format(left_density, right_density, temperature), left_density, right_density, temperature)
+left_density = left_num/half_volume
+right_density = 0
+length = math.pow(half_volume, 1/3)
+temperature = 0.7
+make_file("input/ln{}-rn{}-ld{}-rd{}-T{}.input".format(left_num, right_num, left_density, right_density, temperature), left_num, right_num, left_density, right_density, temperature)
