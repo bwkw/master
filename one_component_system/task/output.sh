4e3bd8f219eee80274c7aa655c0bd8c0f187a77a
S.Naito <na.shota.1139@gmail.com>
2021-11-12 10:59:12 +0900

    [change]実行ファイル変更

--- one_component_system/task/output.sh
+++ one_component_system/task/output.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
 cd $PBS_O_WORKDIR
-mpirun -np 8 /home/apps/lammps/lmp_mpi < input/ln42592-rn2916-ld0.6655-rd0.0455625-T1.0.input > output/ln42592-rn2916-ld0.6655-rd0.0455625-T1.0.output 
+mpirun -np 8 /home/apps/lammps/lmp_mpi < input/ln2916-rn0-ld0.0455625-rd0.0-T1.0.input > output/ln2916-rn0-ld0.0455625-rd0.0-T1.0.output 
