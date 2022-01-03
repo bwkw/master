db68299113b6684723c8548b640ccf6e4235663e
S.Naito <na.shota.1139@gmail.com>
2022-01-03 19:15:16 +0900

    [update]条件変更

--- two_component_system/task/temp.sh
+++ two_component_system/task/temp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
 cd $PBS_O_WORKDIR
-mpirun -np 8 /home/apps/lammps/lmp_serial<input/l0.8-r0.0-T1.0.input > dat/l0.8-r0.0-T1.0.dat
+mpirun -np 8 /home/apps/lammps/lmp_serial<in.melt/ln42592-rn42592-ld0.6655-rd0.6655-T0.2.in
