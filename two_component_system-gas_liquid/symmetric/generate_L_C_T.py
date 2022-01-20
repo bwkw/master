e3c3aaae69490d82fba9141299ad0c47beab4dd7
S.Naito <na.shota.1139@gmail.com>
2022-01-20 16:30:16 +0900

    [add]パラメータ作成処理実装

---
+++ two_component_system-gas_liquid/symmetric/generate_L_C_T.py
@@ -0,0 +1,22 @@
+# パラメータとして、系の長さ（Length）、Aの組成比（CompositionRatio）、温度（Temperature）
+# コマンドライン引数に、系の長さ、Aの組成比分割個数、温度を入れてしまおう
+
+import sys
+import os
+
+def make_file(filename, length, a_composition_ratio, temperature):
+    with open(filename, "w") as f:
+        f.write("length={}\n".format(length))
+        f.write("a_composition_ratio={}\n".format(a_composition_ratio))
+        f.write("temperature={}\n".format(temperature))
+
+length = int(sys.argv[1])
+composition_number = int(sys.argv[2])
+temperature = float(sys.argv[3])
+
+if not os.path.exists("data/param/L{}T{}".format(length, temperature)):
+    os.mkdir("data/param/L{}T{}".format(length, temperature))
+
+for i in range(1, composition_number):
+    a_composition_ratio = (1/composition_number) * int(i)
+    make_file("data/param/L{}T{}/C{}.param".format(length, temperature, a_composition_ratio), length, a_composition_ratio, temperature)
