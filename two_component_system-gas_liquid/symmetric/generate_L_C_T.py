01f481d75550d7cabeb7a2aba6674e79df5baed9
S.Naito <na.shota.1139@gmail.com>
2022-01-20 19:10:47 +0900

    [update]小数桁3桁までに

--- two_component_system-gas_liquid/symmetric/generate_L_C_T.py
+++ two_component_system-gas_liquid/symmetric/generate_L_C_T.py
@@ -1,6 +1,6 @@
 # パラメータファイルを作成
 # 系の長さ（Length）、Aの組成比（CompositionRatio）、温度（Temperature）
-# コマンドライン引数に、系の長さ、Aの組成比分割個数、温度を入れてしまおう
+# コマンドライン引数に、系の長さ、Aの組成比分割個数、温度を入れる
 
 import sys
 import os
@@ -19,5 +19,5 @@ if not os.path.exists("data/param/L{}T{}".format(length, temperature)):
     os.mkdir("data/param/L{}T{}".format(length, temperature))
 
 for i in range(1, composition_number):
-    a_composition_ratio = (1/composition_number) * int(i)
+    a_composition_ratio = round((1/composition_number)*int(i), 3)
     make_file("data/param/L{}T{}/C{}.param".format(length, temperature, a_composition_ratio), length, a_composition_ratio, temperature)
