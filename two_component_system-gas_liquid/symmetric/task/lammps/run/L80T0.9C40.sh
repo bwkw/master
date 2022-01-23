b7ccf5e9f0a8a8a8d6e20a15b2e0650ab36a08a2
S.Naito <na.shota.1139@gmail.com>
2022-01-23 23:02:53 +0900

    [test]

--- two_component_system-gas_liquid/symmetric/task/lammps/run/L80T0.9C40.sh
+++ two_component_system-gas_liquid/symmetric/task/lammps/run/L80T0.9C40.sh
@@ -3,6 +3,8 @@
 #SBATCH -p i8cpu
 #SBATCH -N 1
 #SBATCH -n 128
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=n.shota.1149@gmail.com
 
 source /home/issp/materiapps/intel/lammps/lammpsvars.sh
-srun task/lammps/in.melt/L80T0.9C40inmelt.sh
+
