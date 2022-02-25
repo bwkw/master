7e47f7ae53373cb5e083596417b103e2a2f5b2c7
S.Naito <na.shota.1139@gmail.com>
2022-02-25 09:21:25 +0900

    [add] 温度1.0のデータ追加

--- two_component_system-gas_liquid/symmetric/task/python/run/L80T1.0C40density.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#PBS -l nodes=1:ppn=20
-cd $PBS_O_WORKDIR
-
-mpirun -np 4 --oversubscribe task/cps task/python/density/L80T1.0C40density.sh
--- two_component_system-gas_liquid/symmetric/task/python/run/L80T1.0C40inmelt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#PBS -l nodes=1:ppn=20
-cd $PBS_O_WORKDIR
-
-mpirun -np 4 --oversubscribe task/cps task/python/in.melt/L80T1.0C40inmelt.sh
