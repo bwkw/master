a5075ab9b694c6308ae2ccac36cf96e713533791
S.Naito <na.shota.1139@gmail.com>
2022-03-13 22:34:06 +0900

    [update] 並列数変更

--- two_component_system-gas_liquid/asymmetric/task/lammps/run/L80T1.0C100.sh
+++ two_component_system-gas_liquid/asymmetric/task/lammps/run/L80T1.0C100.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#SBATCH -p F72cpu
-#SBATCH -N 72
-#SBATCH -n 9216
+#SBATCH -p F4cpu
+#SBATCH -N 4
+#SBATCH -n 128
+#SBATCH -c 4
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=n.shota.1139@keio.jp
 
