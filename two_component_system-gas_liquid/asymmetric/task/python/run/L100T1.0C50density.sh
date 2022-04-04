c8f64d63489c4e97db22314b7fbaac609806eade
S.Naito <na.shota.1139@gmail.com>
2022-04-04 21:03:26 +0900

    fix: パーテションの変更

--- two_component_system-gas_liquid/asymmetric/task/python/run/L100T1.0C50density.sh
+++ two_component_system-gas_liquid/asymmetric/task/python/run/L100T1.0C50density.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#SBATCH -p F16cpu
-#SBATCH -N 16
-#SBATCH -n 256
-#SBATCH -c 16
+#SBATCH -p F36cpu
+#SBATCH -N 36
+#SBATCH -n 128
+#SBATCH -c 36
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=n.shota.1139@keio.jp
 
