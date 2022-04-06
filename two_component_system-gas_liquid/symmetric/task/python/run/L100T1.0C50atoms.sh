c6187806d75b5267e6d964ee6fd60ed4bdf26bd6
S.Naito <na.shota.1139@gmail.com>
2022-04-06 12:12:17 +0900

    feat: L100T1.0C50データ追加

--- two_component_system-gas_liquid/symmetric/task/python/run/L100T1.0C50atoms.sh
+++ two_component_system-gas_liquid/symmetric/task/python/run/L100T1.0C50atoms.sh
@@ -2,5 +2,5 @@
 #PBS -l nodes=1:ppn=20
 cd $PBS_O_WORKDIR
 
-mpirun -np 4 --oversubscribe task/cps task/python/atoms/L100T1.0C50atoms.sh
+mpirun -np 4 --oversubscribe task/cps task/python/atoms/L100C50atoms.sh
     
