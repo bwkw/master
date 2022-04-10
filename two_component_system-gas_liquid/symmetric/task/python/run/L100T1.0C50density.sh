710cf3b632ea786f3f33032de36225c1c54d71f7
S.Naito <na.shota.1139@gmail.com>
2022-04-10 09:22:34 +0900

    feat: densityのjobファイル追加

--- two_component_system-gas_liquid/symmetric/task/python/run/L100T1.0C50density.sh
+++ two_component_system-gas_liquid/symmetric/task/python/run/L100T1.0C50density.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#SBATCH -p F36cpu
-#SBATCH -N 36
-#SBATCH -n 128
+#SBATCH -p F72cpu
+#SBATCH -N 72
+#SBATCH -n 256
 #SBATCH -c 36
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=n.shota.1139@keio.jp
