cd2fe16d9b8eb510f6db2249b64a1dde43655342
S.Naito <na.shota.1139@gmail.com>
2022-04-10 15:39:10 +0900

    feat: density_fitting_plt追加

--- two_component_system-gas_liquid/asymmetric_test/task/python/run/L100T1.0C50density_fitting_plt.sh
+++ two_component_system-gas_liquid/asymmetric_test/task/python/run/L100T1.0C50density_fitting_plt.sh
@@ -1,10 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+#PBS -l nodes=1:ppn=20
+cd $PBS_O_WORKDIR
 
-#SBATCH -p F72cpu
-#SBATCH -N 72
-#SBATCH -n 128
-#SBATCH -c 72
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=n.shota.1139@keio.jp
-
-srun task/cps task/python/density_fitting_plt/L100T1.0C50density_fitting_plt.sh
+mpirun -np 4 --oversubscribe task/cps task/python/density_fitting_plt/L100T1.0C50density_fitting_plt.sh
