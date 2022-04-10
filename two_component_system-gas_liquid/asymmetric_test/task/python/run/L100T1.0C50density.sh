84311ca2fce1b24b92849a31f91f89581135a568
Shota Naito <k011706@ohtaka1.issp.u-tokyo.ac.jp>
2022-04-10 09:39:56 +0900

    feat: densityのjobファイル追加

--- two_component_system-gas_liquid/asymmetric_test/task/python/run/L100T1.0C50density.sh
+++ two_component_system-gas_liquid/asymmetric_test/task/python/run/L100T1.0C50density.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#SBATCH -p F72cpu
-#SBATCH -N 72
+#SBATCH -p F36cpu
+#SBATCH -N 36
 #SBATCH -n 128
-#SBATCH -c 72
+#SBATCH -c 36
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=n.shota.1139@keio.jp
 
