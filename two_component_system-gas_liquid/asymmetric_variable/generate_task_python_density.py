b3f58ec8938ce9c89fe46af24ad82f07f5229986
S.Naito <na.shota.1139@gmail.com>
2022-04-13 11:17:47 +0900

    update: lammps実行用ファイル更新

--- two_component_system-gas_liquid/asymmetric_variable/generate_task_python_density.py
+++ two_component_system-gas_liquid/asymmetric_variable/generate_task_python_density.py
@@ -1,19 +1,19 @@
 # task/python/densityファイル作成
-# 系の長さ（Length）、Aの組成比（CompositionRatio）、温度（Temperature）
-# コマンドライン引数に、系の長さ、Aの組成比分割個数、温度を入れる
+# 系の長さ（Length）、Aの組成比（CompositionRatio）、温度（Temperature）、シグマ(Sigma)
+# コマンドライン引数に、系の長さ、Aの組成比分割個数、温度、変数シグマを入れる
 
 import sys
 
-def make_file(filename, length, temperature, a_composition_ratio):
+def make_file(filename, length, temperature, a_composition_ratio, variable_sigma):
     with open(filename, "a") as f:
-        f.write("python3 generate_density.py < data/param/L{}T{}/C{}.param\n".format(length, temperature, a_composition_ratio))
+        f.write("python3 generate_density.py < data/param/L{}T{}S{}/C{}.param\n".format(length, temperature, variable_sigma, a_composition_ratio))
 
 
 length = int(sys.argv[1])
 composition_number = int(sys.argv[2])
 temperature = float(sys.argv[3])
-half_volume = length**3
+variable_sigma = float(sys.argv[4])
 
 for i in range(1, composition_number):
     a_composition_ratio = round((1/composition_number)*int(i), 3)
-    make_file("task/python/density/L{}T{}C{}density.sh".format(length, temperature, composition_number), length, temperature, a_composition_ratio)
+    make_file("task/python/density/L{}T{}C{}S{}density.sh".format(length, temperature, composition_number, variable_sigma), length, temperature, a_composition_ratio, variable_sigma)
