30c1138428b23f2501e4b5687c02e3983704e62a
S.Naito <na.shota.1139@gmail.com>
2022-05-08 14:41:47 +0900

    rename: フォルダ名変更

---
+++ two_component_system-gas_liquid/asymmetric_sigma_variable/generate_density.py
@@ -0,0 +1,101 @@
+# dumpファイルからdensityファイルを作成する
+
+import math
+import os
+import re
+
+
+## dumpファイルを読み込み、二種類の粒子の位置リストを作成する
+def loadfile(filename):
+    with open(filename) as f:
+        ### aは出力する範囲を特定する変数
+        a = 0
+        ### bはdumpを吐いている数(最大6)
+        b = 0
+        for line in f:
+            if re.match("ITEM: ATOMS id type xs ys zs", line):
+                a = 1
+                b += 1
+                continue
+            elif re.match("ITEM: TIMESTEP", line):
+                a = 0
+            if ((a == 1) and ((b >= 2) and (b <= 1001))):
+                line = line.split()
+                type = line[1]
+                x = line[2]
+                if type=="1":
+                    type1_x_list.append(x)
+                else:
+                    type2_x_list.append(x)
+
+## 二種類の粒子の密度リストからdensityファイルを作成する
+def makefile(filename, density_list1, density_list2):
+    with open(filename, "w") as f:
+        for i in range(len(density_list1)):
+            f.write("{} {} {}\n".format(round(float(0.0025)*i, 4), density_list1[i], density_list2[i]))
+
+## 標準入力（paramファイル）からパラメータ取得
+param_dic = {}
+for l in range(4):
+    a, b = input().split("=")
+    param_dic[a] = b
+
+length = int(param_dic["length"])
+a_composition_ratio = float(param_dic["a_composition_ratio"])
+temperature = float(param_dic["temperature"])
+variable_sigma = float(param_dic["variable_sigma"])
+
+half_volume = length**3
+left_s = round(math.pow((half_volume*0.7/4), 1/3))
+right_s = round(math.pow((half_volume*0.02/4), 1/3))
+left_num = left_s**3*4
+right_num = right_s**3*4
+total_num = left_num + right_num
+left_a_num = round(left_num * a_composition_ratio)
+left_b_num = left_num - left_a_num
+right_a_num = round(right_num * a_composition_ratio)
+right_b_num = right_num - right_a_num
+
+type1_x_list = []
+type2_x_list = []
+loadfile("../../../../../../work/k0117/k011706/asymmetric_variable/data/dump.melt/L{}T{}S{}/lan{}-lbn{}-ran{}-rbn{}.dump".format(length, temperature, variable_sigma, left_a_num, left_b_num, right_a_num, right_b_num))
+
+x_interval = 0.0025
+x_interval_num = int(1/float(x_interval))
+type1_density_list = [0]*(x_interval_num)
+type2_density_list = [0]*(x_interval_num)
+
+for i in range(len(type1_x_list)):
+    x = float(type1_x_list[i])
+    if x < 1:
+        density_position = math.floor(float(x)/float(0.0025))
+        type1_density_list[density_position] += 1
+    elif x == 1:
+        type1_density_list[399] += 1
+    else:
+        x -= 1
+        density_position = math.floor(float(x)/float(0.0025))
+        type1_density_list[density_position] += 1
+
+for i in range(len(type2_x_list)):
+    x = float(type2_x_list[i])
+    if x < 1:
+        density_position = math.floor(float(x)/float(0.0025))
+        type2_density_list[density_position] += 1
+    elif x == 1:
+        type2_density_list[399] += 1
+    else:
+        x -= 1
+        density_position = math.floor(float(x)/float(0.0025))
+        type2_density_list[density_position] += 1
+
+type1_density_list = list(map(lambda x: x/(0.0025*half_volume*2*1000), type1_density_list))
+type2_density_list = list(map(lambda x: x/(0.0025*half_volume*2*1000), type2_density_list))
+
+if not os.path.exists('density'):
+    os.mkdir('density')
+if not os.path.exists('density/density'):
+    os.mkdir('density/density')
+if not os.path.exists('density/density/L{}T{}S{}'.format(length, temperature, variable_sigma)):
+    os.mkdir('density/density/L{}T{}S{}'.format(length, temperature, variable_sigma))
+makefile("density/density/L{}T{}S{}/lan{}-lbn{}-ran{}-rbn{}.density".format(length, temperature, variable_sigma, left_a_num, left_b_num, right_a_num, right_b_num), type1_density_list, type2_density_list)
---
+++ two_component_system-gas_liquid/asymmetric_sigma_variable/generate_task_lammps_run.py
@@ -0,0 +1,48 @@
+# task/lammps/runファイル作成
+# 系の長さ（Length）、Aの組成比（CompositionRatio）、温度（Temperature）、シグマ(Sigma)
+# コマンドライン引数に、系の長さ、Aの組成比分割個数、温度、変数シグマを入れる
+
+import math
+import sys
+
+def make_base_file(filename):
+    with open(filename, "w") as f:
+        f.write("#!/bin/sh\n\n")
+        f.write("#SBATCH -p F144cpu\n")
+        f.write("#SBATCH -N 144\n")
+        f.write("#SBATCH -n 18432\n")
+        f.write("#SBATCH --mail-type=ALL\n")
+        f.write("#SBATCH --mail-user=n.shota.1139@keio.jp\n\n")
+        f.write("source /home/issp/materiapps/intel/lammps/lammpsvars.sh\n\n")
+
+def make_file(filename, length, temperature, variable_sigma, left_a_num, left_b_num, right_a_num, right_b_num):
+    with open(filename, "a") as f:
+        f.write("srun --exclusive --mem-per-cpu=1840 -n 256 -c 1 -N 2 lammps < data/in.melt/L{}T{}S{}/lan{}-lbn{}-ran{}-rbn{}.in &\n"
+.format(length, temperature, variable_sigma, left_a_num, left_b_num, right_a_num, right_b_num))
+
+def make_end_file(filename):
+    with open(filename, "a") as f:
+        f.write("wait\n")
+
+
+length = int(sys.argv[1])
+composition_number = int(sys.argv[2])
+temperature = float(sys.argv[3])
+variable_sigma = float(sys.argv[4])
+
+half_volume = length**3
+make_base_file("task/lammps/run/L{}T{}C{}S{}.sh".format(length, temperature, composition_number, variable_sigma))
+left_s = round(math.pow((half_volume*0.7/4), 1/3))
+right_s = round(math.pow((half_volume*0.02/4), 1/3))
+left_num = left_s**3*4
+right_num = right_s**3*4
+total_num = left_num + right_num
+for i in range(1, composition_number):
+    a_composition_ratio = round((1/composition_number)*int(i), 3)
+    left_a_num = round(left_num * a_composition_ratio)
+    left_b_num = left_num - left_a_num
+    right_a_num = round(right_num * a_composition_ratio)
+    right_b_num = right_num - right_a_num
+    make_file("task/lammps/run/L{}T{}C{}S{}.sh".format(length, temperature, composition_number, variable_sigma), length, temperature, variable_sigma, left_a_num, left_b_num, right_a_num, right_b_num)
+
+make_end_file("task/lammps/run/L{}T{}C{}S{}.sh".format(length, temperature, composition_number, variable_sigma))
