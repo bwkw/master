858dd1ae2aa0f4db1e294c78fea04d3e1e2db84a
S.Naito <na.shota.1139@gmail.com>
2022-05-19 11:40:30 +0900

    feat: densityファイル作成

--- three_component_sysytem-gas_liquid/symmetric/task/python/run/L100T1.0C50E1.2density.sh
+++ three_component_sysytem-gas_liquid/symmetric/task/python/run/L100T1.0C50E1.2density.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=n.shota.1139@keio.jp
 
-srun task/cps task/python/density/L100T1.0C50density.sh
+srun task/cps task/python/density/L100T1.0C50E1.2density.sh
