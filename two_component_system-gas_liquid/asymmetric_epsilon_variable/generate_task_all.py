f515100a69c957b08242dc1d4e3abf0d0f262965
S.Naito <na.shota.1139@gmail.com>
2022-06-02 09:35:37 +0900

    feat: データ追加

---
+++ two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_task_all.py
@@ -0,0 +1,68 @@
+# task/lammps/runファイル作成
+# 系の長さ（Length）、温度（Temperature）、組成比分割数（CompositionNumber）、変数イプシロン（Epsilon）、Cの密度（Density）
+# コマンドライン引数に、系の長さ、温度、組成比分割数、変数イプシロン、Cの密度を入れる
+
+import math
+import sys
+import os
+
+def make_lammps_base_file(filename):
+    with open(filename, "w") as f:
+        f.write("#!/bin/sh\n\n")
+        f.write("#SBATCH -p F144cpu\n")
+        f.write("#SBATCH -N 144\n")
+        f.write("#SBATCH -n 18432\n")
+        f.write("#SBATCH --mail-type=ALL\n")
+        f.write("#SBATCH --mail-user=n.shota.1139@keio.jp\n\n")
+        f.write("source /home/issp/materiapps/intel/lammps/lammpsvars.sh\n\n")
+
+def make_lammps_file(filename, length, temperature, variable_epsilon, left_a_num, left_b_num, right_a_num, right_b_num):
+    with open(filename, "a") as f:
+        f.write("srun --exclusive --mem-per-cpu=1840 -n 256 -c 1 -N 2 lammps < data/in.melt/L{}T{}E{}/lan{}-lbn{}-ran{}-rbn{}.in\n".format(length, temperature, variable_epsilon, left_a_num, left_b_num, right_a_num, right_b_num))
+
+def make_lammps_end_file(filename):
+    with open(filename, "a") as f:
+        f.write("wait\n")
+
+def make_atoms_file(filename, length, temperature, a_composition_ratio, variable_epsilon):
+    with open(filename, "a") as f:
+        f.write("python3 generate_atoms.py < data/param/L{}T{}E{}/AC{}.param\n".format(length, temperature, variable_epsilon, a_composition_ratio))
+
+def make_density_file(filename, length, temperature, a_composition_ratio, variable_epsilon):
+    with open(filename, "a") as f:
+        f.write("python3 generate_density.py < data/param/L{}T{}E{}/AC{}.param\n".format(length, temperature, variable_epsilon, a_composition_ratio))
+
+def make_density_fitting_plt_file(filename, length, temperature, a_composition_ratio, variable_epsilon):
+    with open(filename, "a") as f:
+        f.write("python3 generate_density_fitting_plt.py < data/param/L{}T{}E{}/AC{}.param\n".format(length, temperature, variable_epsilon, a_composition_ratio))
+
+def make_inmelt_file(filename, length, temperature, a_composition_ratio, variable_epsilon):
+    with open(filename, "a") as f:
+        f.write("python3 generate_in.melt.py < data/param/L{}T{}E{}/AC{}.param\n".format(length, temperature, variable_epsilon, a_composition_ratio))
+
+length = int(sys.argv[1])
+temperature = float(sys.argv[2])
+composition_number = int(sys.argv[3])
+variable_epsilon = float(sys.argv[4])
+
+
+half_volume = length**3
+make_lammps_base_file("task/lammps/run/L{}T{}CN{}E{}.sh".format(length, temperature, composition_number, variable_epsilon))
+left_s = round(math.pow((half_volume*0.7/4), 1/3))
+right_s = round(math.pow((half_volume*0.02/4), 1/3))
+left_num = left_s**3*4
+right_num = right_s**3*4
+total_num = left_num + right_num
+for i in range(1, composition_number):
+    a_composition_ratio = round((1/composition_number)*int(i), 3)
+    left_a_num = round(left_num * a_composition_ratio)
+    left_b_num = left_num - left_a_num
+    right_a_num = round(right_num * a_composition_ratio)
+    right_b_num = right_num - right_a_num
+    make_lammps_file("task/lammps/run/L{}T{}CN{}E{}.sh".format(length, temperature, composition_number, variable_epsilon), length, temperature, variable_epsilon, left_a_num, left_b_num, right_a_num, right_b_num)
+    make_atoms_file("task/python/atoms/L{}CN{}atoms.sh".format(length, composition_number), length, temperature, a_composition_ratio, variable_epsilon)
+    make_inmelt_file("task/python/in.melt/L{}T{}CN{}E{}inmelt.sh".format(length, temperature, composition_number, variable_epsilon), length, temperature, a_composition_ratio, variable_epsilon)
+    make_density_file("task/python/density/L{}T{}CN{}E{}density.sh".format(length, temperature, composition_number, variable_epsilon), length, temperature, a_composition_ratio, variable_epsilon)
+    make_density_fitting_plt_file("task/python/density_fitting_plt/L{}T{}CN{}E{}density_fitting_plt.sh".format(length, temperature, composition_number, variable_epsilon), length, temperature, a_composition_ratio, variable_epsilon)
+
+make_lammps_end_file("task/lammps/run/L{}T{}CN{}E{}.sh".format(length, temperature, composition_number, variable_epsilon))
---
+++ two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_task_all_run.py
@@ -0,0 +1,47 @@
+# task/lammps/runファイル作成
+# 系の長さ（Length）、温度（Temperature）、組成比分割数（CompositionNumber）、変数イプシロン（Epsilon）、Cの密度（Density）
+# コマンドライン引数に、系の長さ、温度、組成比分割数、変数イプシロン、Cの密度を入れる
+
+import sys
+
+def make_atoms_file(filename, length, composition_number):
+    with open(filename, "a") as f:
+        f.write("#!/bin/bash\n");
+        f.write("#PBS -l nodes=1:ppn=20\n");
+        f.write("cd $PBS_O_WORKDIR\n\n");
+        f.write("mpirun -np 4 --oversubscribe task/cps task/python/atoms/L{}CN{}atoms.sh\n".format(length, composition_number))
+
+def make_inmelt_file(filename, length, temperature, composition_number, variable_epsilon):
+    with open(filename, "a") as f:
+        f.write("#!/bin/bash\n");
+        f.write("#PBS -l nodes=1:ppn=20\n");
+        f.write("cd $PBS_O_WORKDIR\n\n");
+        f.write("mpirun -np 4 --oversubscribe task/cps task/python/in.melt/L{}T{}CN{}E{}inmelt.sh\n".format(length, temperature, composition_number, variable_epsilon))
+
+def make_density_file(filename, length, temperature, composition_number, variable_epsilon):
+    with open(filename, "a") as f:
+        f.write("#!/bin/bash\n");
+        f.write("#SBATCH -p F144cpu\n");
+        f.write("#SBATCH -N 144\n");
+        f.write("#SBATCH -n 576\n");
+        f.write("#SBATCH -c 32\n");
+        f.write("#SBATCH --mail-type=ALL\n");
+        f.write("#SBATCH --mail-user=n.shota.1139@keio.jp\n\n");
+        f.write("srun task/cps task/python/density/L{}T{}CN{}E{}density.sh\n".format(length, temperature, composition_number, variable_epsilon))
+
+def make_density_fitting_plt_file(filename, length, temperature, composition_number, variable_epsilon):
+    with open(filename, "a") as f:
+        f.write("#!/bin/bash\n");
+        f.write("#PBS -l nodes=1:ppn=20\n");
+        f.write("cd $PBS_O_WORKDIR\n\n");
+        f.write("mpirun -np 4 --oversubscribe task/cps task/python/density_fitting_plt/L{}T{}CN{}E{}density_fitting_plt.sh\n".format(length, temperature, composition_number, variable_epsilon))
+
+length = int(sys.argv[1])
+temperature = float(sys.argv[2])
+composition_number = int(sys.argv[3])
+variable_epsilon = float(sys.argv[4])
+
+make_atoms_file("task/python/run/L{}CN{}atoms.sh".format(length, composition_number), length, composition_number)
+make_inmelt_file("task/python/run/L{}T{}CN{}E{}inmelt.sh".format(length, temperature, composition_number, variable_epsilon),length, temperature, composition_number, variable_epsilon)
+make_density_file("task/python/run/L{}T{}CN{}E{}density.sh".format(length, temperature, composition_number, variable_epsilon),length, temperature, composition_number, variable_epsilon)
+make_density_fitting_plt_file("task/python/run/L{}T{}CN{}E{}density_fitting_plt.sh".format(length, temperature, composition_number, variable_epsilon),length, temperature, composition_number, variable_epsilon)
