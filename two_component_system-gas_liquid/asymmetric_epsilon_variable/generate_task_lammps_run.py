4f2367143501fccebc6fd22d88c06d823aaa19b9
S.Naito <na.shota.1139@gmail.com>
2022-06-11 17:54:20 +0900

    feat: data追加

--- two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_task_lammps_run.py
+++ two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_task_lammps_run.py
@@ -17,7 +17,7 @@ def make_base_file(filename):
 
 def make_file(filename, length, temperature, epsilon, left_a_num, left_b_num, right_a_num, right_b_num):
     with open(filename, "a") as f:
-        f.write("srun --exclusive --mem-per-cpu=1840 -n 512 -c 1 -N 4 lammps < data/in.melt/L{}T{}E{}/lan{}-lbn{}-ran{}-rbn{}.in &\n"
+        f.write("srun --exclusive --mem-per-cpu=1840 -n 256 -c 1 -N 2 lammps < data/in.melt/L{}T{}E{}/lan{}-lbn{}-ran{}-rbn{}.in &\n"
 .format(length, temperature, epsilon, left_a_num, left_b_num, right_a_num, right_b_num))
 
 def make_end_file(filename):
--- two_component_system-gas_liquid/asymmetric_epsilon_variable/task/python/run/L80T1.0CN20E1.02density.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p F144cpu
-#SBATCH -N 144
-#SBATCH -n 576
-#SBATCH -c 32
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=n.shota.1139@keio.jp
-
-srun task/cps task/python/density/L80T1.0CN20E1.02density.sh
