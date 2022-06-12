64bcfde29f06072ef013a828f8b8691cb752c019
S.Naito <na.shota.1139@gmail.com>
2022-06-12 13:55:38 +0900

    feat: データ整理

--- two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_in.melt.py
+++ two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_in.melt.py
@@ -21,7 +21,7 @@ def make_file(filename, length, temperature, left_a_num, left_b_num, right_a_num
         f.write("fix 1 all nvt temp {} {} 0.01\n\n".format(temperature, temperature))
         f.write("run 3000000\n\n")
         f.write("dump id all atom 1000 ../../../../../../work/k0117/k011706/asymmetric_epsilon_variable/data/dump.melt/L{}T{}E{}/lan{}-lbn{}-ran{}-rbn{}.dump\n\n".format(length, temperature, variable_epsilon, left_a_num, left_b_num, right_a_num, right_b_num))
-        f.write("run 500000")
+        f.write("run 1000000")
 
 
 ## 標準入力（paramファイル）からパラメータ取得
