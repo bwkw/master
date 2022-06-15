3f5b50f3a7a9bd9580683832b4847e101d11d120
S.Naito <na.shota.1139@gmail.com>
2022-06-15 21:45:30 +0900

    feat: データ作成

--- three_component_sysytem-gas_liquid/symmetric_variable_epsilon_density/task/python/run/L100CN50CD0.02atoms.sh
+++ three_component_sysytem-gas_liquid/symmetric_variable_epsilon_density/task/python/run/L100CN50CD0.02atoms.sh
@@ -3,3 +3,8 @@
 cd $PBS_O_WORKDIR
 
 mpirun -np 4 --oversubscribe task/cps task/python/atoms/L100CN50CD0.02atoms.sh
+#!/bin/bash
+#PBS -l nodes=1:ppn=20
+cd $PBS_O_WORKDIR
+
+mpirun -np 4 --oversubscribe task/cps task/python/atoms/L100CN50CD0.02atoms.sh
