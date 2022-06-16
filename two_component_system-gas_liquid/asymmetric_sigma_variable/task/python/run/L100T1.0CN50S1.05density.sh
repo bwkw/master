96b793855dae1de39cadddc432f3c49e918cf10d
S.Naito <na.shota.1139@gmail.com>
2022-06-16 12:46:38 +0900

    feat: データ追加

---
+++ two_component_system-gas_liquid/asymmetric_sigma_variable/task/python/run/L100T1.0CN50S1.05density.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p F144cpu
+#SBATCH -N 144
+#SBATCH -n 576
+#SBATCH -c 32
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=n.shota.1139@keio.jp
+
+srun task/cps task/python/density/L100T1.0CN50S1.05density.sh
