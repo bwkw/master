a0bb2b9d1a59a84d0ef32275ea5ce6585363ca76
S.Naito <na.shota.1139@gmail.com>
2022-06-18 09:06:19 +0900

    feat: taskファイル追加

--- two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_azeotrope.py
+++ two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_azeotrope.py
@@ -1,6 +1,7 @@
 # densityファイルから共沸点を取得するファイル
 # コマンドライン引数に、系の長さ、Aの組成比分割個数、温度、イプシロンを入れる
 
+import os
 import re
 import sys
 from uncertainties import ufloat
@@ -91,48 +92,5 @@ for parameter in parameters:
 
 make_plt_file("azeotrope/azeotrope_plt/L{}T{}E{}.plt".format(length, temperature, variable_epsilon), length, temperature, variable_epsilon)
 
-# ratio_parameters = []
-# yz_lists = []
-# length = int(sys.argv[1])
-# temperature = float(sys.argv[2])
-# files = os.listdir("density/density/L{}T{}/".format(length, temperature))
-# for filename in files:
-#     result = re.findall(r"\d+", filename)
-#     a_composition_ratio = round((int(result[0]))/(int(result[0])+int(result[1])), 3)
-    
-#     # y = a_gas_density*b_liquid_density
-#     # delta_y = b_liquid_density*a_gas_stdev + a_gas_density*b_liquid_stdev
-#     # z = a_liquid_density*b_gas_density
-#     # delta_z = b_gas_density*a_liquid_stdev + a_liquid_density*b_gas_stdev
-#     # x = y-z
-#     # delta_x = delta_y-delta_z
-#     # yz_list = []
-#     # yz_list.append(a_composition_ratio)
-#     # yz_list.append(y)
-#     # yz_list.append(z)
-#     # yz_list.append(delta_y)
-#     # yz_list.append(delta_z)
-#     # yz_lists.append(yz_list)
-
-#     if not os.path.exists('azeotrope'):
-#         os.mkdir('azeotrope')
-#     if not os.path.exists('azeotrope/gas_liquid_density'):
-#         os.mkdir('azeotrope/gas_liquid_density')
-#     make_ab_gasliquid_file("azeotrope/gas_liquid_density/L{}T{}.dat".format(length, temperature), a_composition_ratio, a_gas_density, a_liquid_density, b_gas_density, b_liquid_density, x)
-#     ratio_parameter = []
-#     ratio_parameter.append(a_composition_ratio)
-#     ratio_parameter.append(x)
-#     ratio_parameter.append(delta_x)
-#     ratio_parameters.append(ratio_parameter)
-
-# yz_lists = sorted(yz_lists, reverse=False, key=lambda x: x[0])
-# for yz_list in yz_lists:
-#     if not os.path.exists('azeotrope/yz'):
-#         os.mkdir('azeotrope/yz')
-#     make_yz_file("azeotrope/yz/L{}T{}.dat".format(length, temperature), yz_list[0], yz_list[1], yz_list[2], yz_list[3], yz_list[4])
-
-# ratio_parameters = sorted(ratio_parameters, reverse=False, key=lambda x: x[0]) 
-# for ratio_parameter in ratio_parameters:
-#     if not os.path.exists('azeotrope/azeotrope'):
-#         os.mkdir('azeotrope/azeotrope')
-#     make_azeotrope_file("azeotrope/azeotrope/L{}T{}.dat".format(length, temperature), ratio_parameter[0], ratio_parameter[1], ratio_parameter[2])
+if not os.path.exists('density/density_fitting_img/L{}T{}E{}'.format(length, temperature, variable_epsilon)):
+    os.mkdir('density/density_fitting_img/L{}T{}E{}'.format(length, temperature, variable_epsilon))
---
+++ two_component_system-gas_liquid/asymmetric_epsilon_variable/task/python/run/L100T1.0CN50E0.975density_fitting_plt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=20
+cd $PBS_O_WORKDIR
+
+mpirun -np 4 --oversubscribe task/cps task/python/density_fitting_plt/L100T1.0CN50E0.975density_fitting_plt.sh
