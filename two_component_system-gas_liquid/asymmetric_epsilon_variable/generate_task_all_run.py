b7565be794236d5229b7f8bdc4bf88c5642e700c
S.Naito <na.shota.1139@gmail.com>
2022-06-30 09:01:34 +0900

    feat: azeotrope追加

--- two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_task_all_run.py
+++ two_component_system-gas_liquid/asymmetric_epsilon_variable/generate_task_all_run.py
@@ -2,6 +2,7 @@
 # 系の長さ（Length）、温度（Temperature）、組成比分割数（CompositionNumber）、変数イプシロン（Epsilon）、Cの密度（Density）
 # コマンドライン引数に、系の長さ、温度、組成比分割数、変数イプシロン、Cの密度を入れる
 
+import os
 import sys
 
 def make_atoms_file(filename, length, composition_number):
@@ -45,3 +46,6 @@ variable_epsilon = float(sys.argv[4])
 make_inmelt_file("task/python/run/L{}T{}CN{}E{}inmelt.sh".format(length, temperature, composition_number, variable_epsilon),length, temperature, composition_number, variable_epsilon)
 make_density_file("task/python/run/L{}T{}CN{}E{}density.sh".format(length, temperature, composition_number, variable_epsilon),length, temperature, composition_number, variable_epsilon)
 make_density_fitting_plt_file("task/python/run/L{}T{}CN{}E{}density_fitting_plt.sh".format(length, temperature, composition_number, variable_epsilon),length, temperature, composition_number, variable_epsilon)
+
+if not os.path.exists('density/density_fitting_img/L{}T{}E{}'.format(length, temperature, variable_epsilon)):
+    os.mkdir('density/density_fitting_img/L{}T{}E{}'.format(length, temperature, variable_epsilon))
