90ce3fd0c9d46ca8f24151900ba2b4afb8d57595
S.Naito <na.shota.1139@gmail.com>
2022-07-14 15:29:33 +0900

    feat: data追加

--- three_component_sysytem-gas_liquid/symmetric_variable_epsilon_density/for_pressure/L100T1.0CN50E1.05CD0.01.sh
+++ three_component_sysytem-gas_liquid/symmetric_variable_epsilon_density/for_pressure/L100T1.0CN50E1.05CD0.01.sh
@@ -8,6 +8,6 @@
 
 source /home/issp/materiapps/intel/lammps/lammpsvars.sh
 
-srun --exclusive --mem-per-cpu=1840 -n 256 -c 1 -N 2 lammps < data/in.melt/L100T1.0E1.05CD0.01/lan292084-lbn403355-lcn7025-ran8171-rbn11284-rcn197.in &
-srun --exclusive --mem-per-cpu=1840 -n 256 -c 1 -N 2 lammps < data/in.melt/L100T1.0E1.05CD0.01/lan305993-lbn389446-lcn7025-ran8560-rbn10895-rcn197.in &
+srun --exclusive --mem-per-cpu=1840 -n 256 -c 1 -N 2 lammps < data/in.melt/L100T1.0E1.05CD0.01/lan292084-lbn403355-lcn7025-ran8171-rbn11284-rcn197.in > data/pressure/L100T1.0E1.05CD0.01/lan292084-lbn403355-lcn7025-ran8171-rbn11284-rcn197.pressure &
+srun --exclusive --mem-per-cpu=1840 -n 256 -c 1 -N 2 lammps < data/in.melt/L100T1.0E1.05CD0.01/lan305993-lbn389446-lcn7025-ran8560-rbn10895-rcn197.in > data/pressure/L100T1.0E1.05CD0.01/lan305993-lbn389446-lcn7025-ran8560-rbn10895-rcn197.pressure &
 wait
