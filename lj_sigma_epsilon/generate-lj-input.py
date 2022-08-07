dceb864be7b2b52be6c4c39256698b3df186325b
S.Naito <na.shota.1139@gmail.com>
2022-08-07 15:10:20 +0900

    feat: ljのポテンシャル依存出力ファイル作成

--- lj_sigma_epsilon/generate-lj-input.py
+++ lj_sigma_epsilon/generate-lj-input.py
@@ -6,13 +6,13 @@ def make_file(filename, distance):
         f.write("atom_style atomic\n\n")
         f.write("read_data lj-atoms/lj{}.atoms\n\n".format(distance))
         f.write("mass 1 1.0\n\n")
-        f.write("pair_style lj/cut 2.5\n")
+        f.write("pair_style lj/cut 3.0\n")
         f.write("pair_modify shift yes\n")
-        f.write("pair_coeff 1 1 1.0 1.0 2.5\n\n")
+        f.write("pair_coeff 1 1 1.0 1.0 3.0\n\n")
         f.write("thermo 1\n")
         f.write("thermo_style custom pe\n\n")
         f.write("run 0")
 
 for i in range(201):
     r = format((0.5 + 0.01*i),'.3f')
-    make_file("lj-atoms/lj{}.input".format(r), r)
+    make_file("lj-input/normal/lj{}.input".format(r), r)
--- lj_sigma_epsilon/generate-lj-task.py
+++ lj_sigma_epsilon/generate-lj-task.py
@@ -3,4 +3,4 @@ filename = "task-lj.sh"
 for i in range(201):
     r = format((0.5 + 0.01*i),'.3f')
     with open(filename, "a") as f:
-        f.write("lmp_serial < lj-input/lj{}.input > lj-dat/lj{}.dat\n".format(r, r))
+        f.write("lmp_serial < lj-input/normal/lj{}.input > lj-dat/normal/lj{}.dat\n".format(r, r))
