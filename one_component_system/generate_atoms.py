71ac672821c0c65e0fa251b972450733a3b3728e
S.Naito <na.shota.1139@gmail.com>
2022-08-22 13:56:28 +0900

    feat: 単成分シミュレーション

--- one_component_system/generate_atoms.py
+++ one_component_system/generate_atoms.py
@@ -15,39 +15,28 @@ class Atom:
 
 
 def add_ball(atoms, left_num, right_num, length):
-    r = length
-    volume = r**3
     left_n = round((left_num/4)**(1/3))
-    print(left_n)
-    right_n = round((right_num/4)**(1/3))
-    left_density = left_num/volume
-    right_density = right_num/volume
-    left_s = r/left_n
-    if right_n != 0:
-        right_s = r/right_n
-    else:
-        right_s = 0
+    left_s = length/left_n
     left_h = left_s/2
-    right_h = right_s/2
-    xvel_list = [0] * (4*(left_n**3+right_n**3))
-    yvel_list = [0] * (4*(left_n**3+right_n**3))
-    zvel_list = [0] * (4*(left_n**3+right_n**3))
-    print(len(xvel_list))
-    for i in range(len(xvel_list)):
+    left_xvel_list = [0] * (left_num)
+    left_yvel_list = [0] * (left_num)
+    left_zvel_list = [0] * (left_num)
+    for i in range(len(left_xvel_list)):
         random_x = random.uniform(-1, 1)
-        xvel_list[i] = random_x
-    for i in range(len(yvel_list)):
+        left_xvel_list[i] = random_x
+    for i in range(len(left_yvel_list)):
         random_y = random.uniform(-1, 1) 
-        yvel_list[i] = random_y
-    for i in range(len(zvel_list)):
+        left_yvel_list[i] = random_y
+    for i in range(len(left_zvel_list)):
         random_z = random.uniform(-1, 1)
-        zvel_list[i] = random_z
-    xvel_list_av = sum(xvel_list)/len(xvel_list)
-    yvel_list_av = sum(yvel_list)/len(yvel_list)
-    zvel_list_av = sum(zvel_list)/len(zvel_list)  
-    xvel_list = list(map(lambda x: x - xvel_list_av, xvel_list))
-    yvel_list = list(map(lambda y: y - yvel_list_av, yvel_list))
-    zvel_list = list(map(lambda z: z - zvel_list_av, zvel_list))
+        left_zvel_list[i] = random_z
+    left_xvel_list_av = sum(left_xvel_list)/len(left_xvel_list)
+    left_yvel_list_av = sum(left_yvel_list)/len(left_yvel_list)
+    left_zvel_list_av = sum(left_zvel_list)/len(left_zvel_list)  
+    left_xvel_list = list(map(lambda x: x - left_xvel_list_av, left_xvel_list))
+    left_yvel_list = list(map(lambda y: y - left_yvel_list_av, left_yvel_list))
+    left_zvel_list = list(map(lambda z: z - left_zvel_list_av, left_zvel_list))
+
     num=0
     for ix in range(0,left_n):
         for iy in range(0,left_n):
@@ -55,23 +44,47 @@ def add_ball(atoms, left_num, right_num, length):
                 x = ix*left_s
                 y = iy*left_s
                 z = iz*left_s
-                atoms.append(Atom(x, y, z, xvel_list[num], yvel_list[num], zvel_list[num]))
-                atoms.append(Atom(x+left_h, y+left_h, z, xvel_list[num+1], yvel_list[num+1], zvel_list[num+1]))
-                atoms.append(Atom(x+left_h, y, z+left_h, xvel_list[num+2], yvel_list[num+2], zvel_list[num+2]))
-                atoms.append(Atom(x, y+left_h, z+left_h, xvel_list[num+3], yvel_list[num+3], zvel_list[num+3]))
+                atoms.append(Atom(x, y, z, left_xvel_list[num], left_yvel_list[num], left_zvel_list[num]))
+                atoms.append(Atom(x+left_h, y+left_h, z, left_xvel_list[num+1], left_yvel_list[num+1], left_zvel_list[num+1]))
+                atoms.append(Atom(x+left_h, y, z+left_h, left_xvel_list[num+2], left_yvel_list[num+2], left_zvel_list[num+2]))
+                atoms.append(Atom(x, y+left_h, z+left_h, left_xvel_list[num+3], left_yvel_list[num+3], left_zvel_list[num+3]))
                 num+=4
+
+    right_n = round((right_num/4)**(1/3))
+    right_s = length/right_n
+    right_h = right_s/2
+    right_xvel_list = [0] * (right_num)
+    right_yvel_list = [0] * (right_num)
+    right_zvel_list = [0] * (right_num)
+    for i in range(len(right_xvel_list)):
+        random_x = random.uniform(-1, 1)
+        right_xvel_list[i] = random_x
+    for i in range(len(right_yvel_list)):
+        random_y = random.uniform(-1, 1) 
+        right_yvel_list[i] = random_y
+    for i in range(len(right_zvel_list)):
+        random_z = random.uniform(-1, 1)
+        right_zvel_list[i] = random_z
+    right_xvel_list_av = sum(right_xvel_list)/len(right_xvel_list)
+    right_yvel_list_av = sum(right_yvel_list)/len(right_yvel_list)
+    right_zvel_list_av = sum(right_zvel_list)/len(right_zvel_list)  
+    right_xvel_list = list(map(lambda x: x - right_xvel_list_av, right_xvel_list))
+    right_yvel_list = list(map(lambda y: y - right_yvel_list_av, right_yvel_list))
+    right_zvel_list = list(map(lambda z: z - right_zvel_list_av, right_zvel_list))
+
+    num=0
     for ix in range(0,right_n):
         for iy in range(0,right_n):
             for iz in range(0,right_n):
-                x = r + ix*right_s
+                x = ix*right_s
                 y = iy*right_s
                 z = iz*right_s
-                atoms.append(Atom(x, y, z, xvel_list[num], yvel_list[num], zvel_list[num]))
-                atoms.append(Atom(x+right_h, y+right_h, z, xvel_list[num+1], yvel_list[num+1], zvel_list[num+1]))
-                atoms.append(Atom(x+right_h, y, z+right_h, xvel_list[num+2], yvel_list[num+2], zvel_list[num+2]))
-                atoms.append(Atom(x, y+right_h, z+right_h, xvel_list[num+3], yvel_list[num+3], zvel_list[num+3]))
+                atoms.append(Atom(x, y, z, right_xvel_list[num], right_yvel_list[num], right_zvel_list[num]))
+                atoms.append(Atom(x+right_h, y+right_h, z, right_xvel_list[num+1], right_yvel_list[num+1], right_zvel_list[num+1]))
+                atoms.append(Atom(x+right_h, y, z+right_h, right_xvel_list[num+2], right_yvel_list[num+2], right_zvel_list[num+2]))
+                atoms.append(Atom(x, y+right_h, z+right_h, right_xvel_list[num+3], right_yvel_list[num+3], right_zvel_list[num+3]))
                 num+=4
-                
+
 def make_file(filename, atoms, length):
     with open(filename, "w") as f:
         f.write("Position Data\n\n")
@@ -89,15 +102,17 @@ def make_file(filename, atoms, length):
         for i, a in enumerate(atoms):
             f.write("{} {} {} {}\n".format(i+1, a.vx, a.vy, a.vz))
 
-half_volume = 40*40*40
+
 atoms = []
-left_num = 22*22*22*4
-right_num = 0
-left_density = left_num/half_volume
-right_density = 0
-length = math.pow(half_volume, 1/3)
+length = 50
+half_volume = length**3
+left_s = round(math.pow((half_volume*0.7/4), 1/3))
+right_s = round(math.pow((half_volume*0.02/4), 1/3))
+left_num = left_s**3*4
+right_num = right_s**3*4
+
 add_ball(atoms, left_num, right_num, length)
-make_file("atoms/ln{}-rn{}-ld{}-rd{}.atoms".format(left_num, right_num, left_density, right_density), atoms, length)
+make_file("atoms/ln{}-rn{}.atoms".format(left_num, right_num), atoms, length)
 
 #速度平均が≒0になっていることの確認
 sum_a_vx = 0
--- one_component_system/generate_input.py
+++ one_component_system/generate_input.py
@@ -1,11 +1,11 @@
 import math
 
-def make_file(filename, left_num, right_num, left_density, right_density, temperature):
+def make_file(filename, left_num, right_num, temperature):
     with open(filename, "w") as f:
         f.write("units lj\n")
         f.write("atom_style atomic\n")
         f.write("boundary p p p\n\n")
-        f.write("read_data atoms/ln{}-rn{}-ld{}-rd{}.atoms\n\n".format(left_num, right_num, left_density, right_density))
+        f.write("read_data atoms/ln{}-rn{}.atoms\n\n".format(left_num, right_num))
         f.write("mass 1 1.0\n\n")
         f.write("reset_timestep  0\n")
         f.write("timestep        0.001\n\n")
@@ -15,15 +15,14 @@ def make_file(filename, left_num, right_num, left_density, right_density, temper
         f.write("run 200000\n\n")
         f.write("thermo 100\n")
         f.write("thermo_style custom time temp press\n\n")
-        f.write("dump id all atom 100 dump.melt/ln{}-rn{}-ld{}-rd{}-T{}.dump\n\n".format(left_num, right_num, left_density, right_density, temperature))
+        f.write("dump id all atom 100 dump.melt/ln{}-rn{}-T{}.dump\n\n".format(left_num, right_num, temperature))
         f.write("run 5000")
 
-
-half_volume = 40*40*40
-left_num = 9*9*9*4
-right_num = 9*9*9*4
-left_density = left_num/half_volume
-right_density = right_num/half_volume
-length = math.pow(half_volume, 1/3)
+length = 50
+half_volume = length**3
+left_s = round(math.pow((half_volume*0.7/4), 1/3))
+right_s = round(math.pow((half_volume*0.02/4), 1/3))
+left_num = left_s**3*4
+right_num = right_s**3*4
 temperature = 1.0
-make_file("input/ln{}-rn{}-ld{}-rd{}-T{}.input".format(left_num, right_num, left_density, right_density, temperature), left_num, right_num, left_density, right_density, temperature)
+make_file("input/ln{}-rn{}-T{}.input".format(left_num, right_num, temperature), left_num, right_num, temperature)
