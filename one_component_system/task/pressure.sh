7ee389f9945e03ea90d1348ff45c7ab7ef3cbc32
S.Naito <na.shota.1139@gmail.com>
2022-08-22 14:20:25 +0900

    feat: 単成分シミュレーション

--- one_component_system/task/pressure.sh
+++ one_component_system/task/pressure.sh
@@ -1,2 +1,2 @@
-./lmp_serial < input/ln87808-rn2916-T1.0.input > output/ln87808-rn2916-T1.0.output
-
+./lmp_serial < input/ln87808-rn2916-T1.0-sigma1.0.input > output/ln87808-rn2916-T1.0-sigma1.0.output
+./lmp_serial < input/ln87808-rn2916-T1.0-sigma1.2.input > output/ln87808-rn2916-T1.0-sigma1.2.output
