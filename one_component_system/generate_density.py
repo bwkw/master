37aae9b84657f3331542630ca81cb183419a0d24
S.Naito <na.shota.1139@gmail.com>
2022-10-16 21:22:00 +0900

    feat: 単成分系にfitting用フォルダ作成

--- one_component_system/generate_density.py
+++ one_component_system/generate_density.py
@@ -16,23 +16,26 @@ def loadfile(filename):
                 continue
             elif re.match("ITEM: TIMESTEP", line):
                 a = 0
-            if ((a == 1) and ((b >= 2) and (b <= 6))):
+            if ((a == 1) and ((b >= 2) and (b <= 51))):
                 line = line.split()
                 x = line[2]
                 x_list.append(x)
-        
+
 def makefile(filename, length):
     with open(filename, "w") as f:
         for i in range(len(density_list)):
             f.write("{} {}\n".format(round(float(0.001)*i, 3), density_list[i]))
 
-half_volume = 40*40*40
-left_num = 22*22*22*4
-right_num = 0
+length = 50
+half_volume = length**3
+left_s = round(math.pow((half_volume*0.6/4), 1/3))
+right_s = round(math.pow((half_volume*0.08/4), 1/3))
+left_num = left_s**3*4
+right_num = right_s**3*4
 left_density = left_num/half_volume
-right_density = 0
-length = math.pow(half_volume, 1/3)
-temperature = 0.9
+right_density = right_num/half_volume
+temperature = 1.0
+
 line_list = []
 x_list = []
 loadfile("dump.melt/ln{}-rn{}-ld{}-rd{}-T{}.dump".format(left_num, right_num, left_density, right_density, temperature))
@@ -43,14 +46,16 @@ density_list = [0]*(x_interval_num)
 for i in range(len(x_list)):
     x = float(x_list[i])
     if x < 1:
-        density_position = math.floor(x/float(0.001))
+        density_position = math.floor(float(x)/float(0.001))
         density_list[density_position] += 1
     elif x == 1:
         density_list[-1] += 1
+    else:
+        x -= 1
+        density_position = math.floor(float(x)/float(0.001))
+        density_list[density_position] += 1
 
-print(sum(density_list))
-
-density_list = list(map(lambda x: x/(0.001*length**3*2*5), density_list))
+density_list = list(map(lambda x: x/(0.001*half_volume*2*50), density_list))
 
 makefile("density/ln{}-rn{}-ld{}-rd{}-T{}.density".format(left_num, right_num, left_density, right_density, temperature), length)
 
