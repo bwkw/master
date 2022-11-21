40da52de3ab8402b695cd2e00067fbd9bbb45a68
S.Naito <na.shota.1139@gmail.com>
2022-11-21 21:23:35 +0900

    feat: 気相密度と液相密度の差分を出力するように

--- two_component_system-gas_liquid/symmetric/generate_azeotrope.py
+++ two_component_system-gas_liquid/symmetric/generate_azeotrope.py
@@ -51,7 +51,7 @@ def make_ab_gasliquid_file(filename, a_composition_ratio, a_gas_density, a_liqui
 
 def make_b_gas_liquid_concentration_file(filename, b_gas_concentration, b_liquid_concentration):
     with open(filename, "a") as f:
-        f.write("{} {}\n".format(b_gas_concentration, b_liquid_concentration))
+        f.write("{} {} {}\n".format(b_gas_concentration, b_liquid_concentration, b_gas_concentration-b_liquid_concentration))
 
 def make_yz_file(filename, a_composition_ratio, Y, Z):
     with open(filename, "a") as f:
@@ -76,11 +76,11 @@ for parameter in parameters:
     b_gas_concentration = b_gas_density / (a_gas_density + b_gas_density)
     b_liquid_concentration = b_liquid_density / (a_liquid_density + b_liquid_density)
     make_b_gas_liquid_concentration_file("azeotrope/b_gas_liquid_concentration/L{}T{}.dat".format(length, temperature), b_gas_concentration, b_liquid_concentration)
-    Y = a_gas_density * b_liquid_density
-    Z = a_liquid_density * b_gas_density
-    X = Y-Z
-    make_azeotrope_file("azeotrope/azeotrope/L{}T{}.dat".format(length, temperature), a_composition_ratio, X)
-    make_yz_file("azeotrope/yz/L{}T{}.dat".format(length, temperature), a_composition_ratio, Y, Z)
+    # Y = a_gas_density * b_liquid_density
+    # Z = a_liquid_density * b_gas_density
+    # X = Y-Z
+    # make_azeotrope_file("azeotrope/azeotrope/L{}T{}.dat".format(length, temperature), a_composition_ratio, X)
+    # make_yz_file("azeotrope/yz/L{}T{}.dat".format(length, temperature), a_composition_ratio, Y, Z)
 
 
 # ratio_parameters = []
