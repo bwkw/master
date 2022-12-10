eeabc4d47c9246b1c80b2856c454f46aece62b2d
Shota Naito <k011706@ohtaka1.issp.u-tokyo.ac.jp>
2022-12-10 09:12:38 +0900

    feat: 密度出力用ファイル修正

--- three_component_sysytem-gas_liquid/symmetric_variable_epsilon_density/task/python/run/L100T1.0CN50E1.05CD0.014density.sh
+++ three_component_sysytem-gas_liquid/symmetric_variable_epsilon_density/task/python/run/L100T1.0CN50E1.05CD0.014density.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -p B144cpu
-#SBATCH -N 144
-#SBATCH -n 576
+#SBATCH -p F4cpu
+#SBATCH -N 2
+#SBATCH -n 8
 #SBATCH -c 32
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=n.shota.1139@keio.jp
